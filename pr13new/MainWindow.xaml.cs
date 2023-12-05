cfc96f3b7945e9229899f71427d96b5dbbd56868
исп-31 <-31@LAB17-13>
2023-12-05 14:33:06 +0300

    Добавьте файлы проекта.

---
+++ pr13new/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pr13new
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();           
+        }
+
+        private void exitClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void infoClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Выполнено Кульковой Ангелиной.\r\n Дана вещественная матрица А(M, N). Строку, содержащий максимальный элемент, поменять местами со строкой, содержащей минимальный элемент.");
+        }
+
+        private void rasClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ochClick(object sender, RoutedEventArgs e)
+        {
+            dgRes.Items.Clear();
+        }
+    }
+}
