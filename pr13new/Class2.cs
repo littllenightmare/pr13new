6bcabc8457ccfdbed66e1d8b27ad9abcad45ad7e
исп-31 <-31@LAB17-13>
2023-12-06 10:47:19 +0300

    Осталось немного

---
+++ pr13new/Class2.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr13new
+{
+    public class Massiv
+    {
+        /// <summary>
+        /// Заполнение массива рандомом
+        /// </summary>
+        /// <param name="mas">массив</param>
+        /// <param name="column">столбцы</param>
+        /// <param name="randMax">максимальный рандом</param>
+        public static void InitMas(out int[] mas, int column, int randMax)
+        {
+            Random random = new Random();
+            mas = new int[column];
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = random.Next(randMax);
+            }
+        }
+        /// <summary>
+        /// Суммирование элементов массива
+        /// </summary>
+        /// <param name="mas">массив</param>
+        /// <returns>сумму</returns>
+        public static int SumMas(int[] mas)
+        {
+            int sum = 0;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                sum += mas[i];
+            }
+            return sum;
+        }
+        /// <summary>
+        /// Сохранение массива
+        /// </summary>
+        /// <param name="mas">массив</param>
+        public static void SaveMas(int[] mas)
+        {
+            StreamWriter sw = new StreamWriter("Ìàññèâ.txt");
+            sw.WriteLine(mas.Length);
+            for (int i = 0; i < mas.Length; i++)
+            {
+                sw.WriteLine(mas[i]);
+            }
+            sw.Close();
+        }
+        /// <summary>
+        /// Добавление массива
+        /// </summary>
+        /// <param name="mas">массив</param>
+        public static void OpenMas(out int[] mas)
+        {
+            StreamReader sr = new StreamReader("Ìàññèâ.txt");
+            int len = Convert.ToInt32(sr.ReadLine());
+            mas = new int[len];
+            for (int i = 0; i < len; i++)
+            {
+                mas[i] = Convert.ToInt32(sr.ReadLine());
+            }
+            sr.Close();
+        }
+        /// <summary>
+        /// Сохранение матрицы
+        /// </summary>
+        /// <param name="matr">матрица</param>
+        public static void SaveMatr(int[,] matr)
+        {
+            StreamWriter sw = new StreamWriter("Ìàòðèöà.txt");
+            int rows = matr.GetLength(0);
+            int columns = matr.GetLength(1);
+            sw.WriteLine($"{rows}");
+            sw.WriteLine($"{columns}");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    sw.Write(matr[i, j] + " ");
+                }
+                sw.WriteLine();
+            }
+            sw.Close();
+        }
+        /// <summary>
+        /// Добавление матрицы
+        /// </summary>
+        /// <param name="matr">матрица</param>
+        public static void OpenMatr(out int[,] matr)
+        {
+            StreamReader sr = new StreamReader("Ìàòðèöà.txt");
+            int rows = Convert.ToInt32(sr.ReadLine());
+            int columns = Convert.ToInt32(sr.ReadLine());
+            matr = new int[rows, columns];
+            for (int i = 0; i < rows; i++)
+            {
+                string[] line = sr.ReadLine().Split(' ');
+                for (int j = 0; j < columns; j++)
+                {
+                    matr[i, j] = int.Parse(line[j]);
+                }
+            }
+            sr.Close();
+        }
+    }
+}
--- pr13new/MainWindow.xaml.cs
+++ pr13new/MainWindow.xaml.cs
@@ -21,41 +21,66 @@ namespace pr13new
     /// </summary>
     public partial class MainWindow : Window
     {
+        int[,] matr;
         public MainWindow()
         {
             InitializeComponent();
-            sizematr.Content = $"Размер матрицы: {dgDano.ActualHeight}, {dgDano.ActualWidth}";
         }
 
-        private void exitClick(object sender, RoutedEventArgs e)
+        private void exitClick(object sender, RoutedEventArgs e)//кнопка выхода
         {
             this.Close();
         }
 
-        private void infoClick(object sender, RoutedEventArgs e)
+        private void infoClick(object sender, RoutedEventArgs e)//кнопка вывода информации
         {
             MessageBox.Show("Выполнено Кульковой Ангелиной.\r\n Дана вещественная матрица А(M, N). Строку, содержащий максимальный элемент, поменять местами со строкой, содержащей минимальный элемент.");
         }
 
-        private void ochClick(object sender, RoutedEventArgs e)
+        private void ochClick(object sender, RoutedEventArgs e)//кнопка очистки матрицы результата
         {
             dgRes.Items.Clear();
         }
 
-        private void CreateCLick(object sender, RoutedEventArgs e)
+        private void CreateCLick(object sender, RoutedEventArgs e)//Кнопка создания матрицы
         {
-            int m,n,a=0,b=0,min=10000,max=0;
-            if (Int32.TryParse(tbM.Text, out m) == true & Int32.TryParse(tbN.Text, out n) == true)
+            int m, n;
+            if (Int32.TryParse(tbM.Text, out m) == true & Int32.TryParse(tbN.Text, out n) == true)//проверяем текстбоксы на верность
             {
                 Random rnd = new Random();
-                int[,] matr = new int[m, n];        
-                for(int i=0; i<m; i++)
+                matr = new int[m, n];        
+                for(int i=0; i<m; i++)//заполняем матрицу рандомными числами
                 {
                     for(int j=0; j<n; j++)
                     {
-                        matr[m, n] = rnd.Next(1, 10);
+                        matr[i, j] = rnd.Next(1, 10);
                     }
                 }
+                dgDano.ItemsSource = VisualArray.ToDataTable(matr).DefaultView;
+                sizematr.Content = $"Размер матрицы: {m}, {n}";
+            }
+            else MessageBox.Show("Введите корректные данные");//если данных нет или они неправильные
+        }
+
+        private void Danocelleditending(object sender, DataGridCellEditEndingEventArgs e)//при изменении ячеек матрицы исходных данных очищает матрицу результат
+        {
+            dgRes.ItemsSource = null;
+            int IndexColumn = e.Column.DisplayIndex;
+            int IndexRow = e.Column.DisplayIndex;
+            matr[IndexColumn, IndexRow] = Convert.ToInt32(((TextBox)e.EditingElement).Text);
+        }
+
+        private void Createtch(object sender, TextChangedEventArgs e)//При изменении текстбоксов создания матрицы очищает матрицы
+        {
+            dgRes.ItemsSource = null;
+            dgDano.ItemsSource = null;
+        }
+
+        private void rasClick(object sender, RoutedEventArgs e)
+        {
+            int m, n, a = 0, b = 0, min = 10000, max = 0;        
+            if (Int32.TryParse(tbM.Text, out m) == true & Int32.TryParse(tbN.Text, out n) == true)//проверяем текстбоксы на верность
+            {
                 for (int i = 0; i < m; i++)
                 {
                     for (int j = 0; j < n; j++)
@@ -68,32 +93,30 @@ namespace pr13new
                         if (matr[i, j] > max)
                         {
                             b = i;
-                            max= matr[i, j];
+                            max = matr[i, j];
                         }
                     }
                 }
-                for (int i = 0; i < m; i++)
-                {
                     for (int j = 0; j < n; j++)
                     {
-                        int c = a;
+                        int c = matr[a,j];
                         matr[a, j] = matr[b, j];
-                        matr[b, j] = matr[c, j];
+                        matr[b, j] = c;
                     }
-                }
+                dgRes.ItemsSource = VisualArray.ToDataTable(matr).DefaultView;
             }
-            else MessageBox.Show("Введите корректные данные");
         }
 
-        private void Danocelleditending(object sender, DataGridCellEditEndingEventArgs e)
+        private void SaveClick(object sender, RoutedEventArgs e)
         {
-            dgRes.Items.Clear();
+            Massiv.SaveMatr(matr);
+            Openbtn.IsEnabled = true;
         }
 
-        private void Createtch(object sender, TextChangedEventArgs e)
+        private void DownloadClick(object sender, RoutedEventArgs e)
         {
-            dgRes.Items.Clear();
-            dgDano.Items.Clear();
+            Massiv.OpenMatr(out matr);
+            dgDano.ItemsSource = VisualArray.ToDataTable(matr).DefaultView;
         }
     }
 }
